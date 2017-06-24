ea6bd3b49f55592729457e1bf88b3e36d8063993
Steve <stevecortis@live.com>
2017-06-24 15:05:18 +0200

    Added two new components

---
+++ app/components/Countdown.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+// Stateless function, this is only used when Components have only the render function
+var Countdown = (props) => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Countdown</h1>
+    </div>
+  )
+};
+
+module.exports = Countdown;
---
+++ app/components/Timer.jsx
@@ -0,0 +1,12 @@
+var React = require('react');
+
+// Stateless function, this is only used when Components have only the render function
+var Timer = (props) => {
+  return (
+    <div>
+      <h1 className="text-center page-title">Timer</h1>
+    </div>
+  )
+};
+
+module.exports = Timer;
